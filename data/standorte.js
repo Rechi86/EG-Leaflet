459e1e06988264add5cb9e9e88e28bf48b8daf7c
Rechi86 <34352015+Rechi86@users.noreply.github.com>
2021-09-06 17:46:11 +0200

    Innitial commit

---
+++ data/standorte.js
@@ -0,0 +1,11 @@
+var standorte = {
+    "type": "FeatureCollection",
+    "name": "standorte",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        { "type": "Feature", "properties": { "id": 1, "Station": "Schauraum", "stateId": "map-schauraum" }, "geometry": { "type": "Point", "coordinates": [14.77598153490295, 47.94649314001705] } },
+
+        { "type": "Feature", "properties": { "id": 2, "Station": "Treffpunkt & Erlebnisraum", "stateId": "map-erlebnisraum" }, "geometry": { "type": "Point", "coordinates": [14.768511452169896, 47.94929703801917] } },
+
+    ]
+}

28d01dd2c6a21b579fef69ffc4859910d83d8990
Rechi86 <34352015+Rechi86@users.noreply.github.com>
2021-09-06 19:53:09 +0200

    Geojson new features

---
+++ data/geojson.js
@@ -0,0 +1,40 @@
+standorte2 = {
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "name": "Punkt 1",
+                "party": "Democrat"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    14.777555465698242,
+                    47.956737580440105
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Punkt 2", "party": "Democrat" },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    14.784464836120605,
+                    47.957772211632694
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": { "name": "Punkt 3", "party": "Republican" },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    14.784808158874512,
+                    47.9554442623138
+                ]
+            }
+        }
+    ]
+};
